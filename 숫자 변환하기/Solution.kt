dea03621cee697b5b07ab84f1dd1e1cbade51349
heesoo-park <80674868+heesoo-park@users.noreply.github.com>
2023-11-28 10:56:17 +0900

    Update Solution.kt

--- 숫자 변환하기/Solution.kt
+++ 숫자 변환하기/Solution.kt
@@ -62,7 +62,7 @@ class Solution {
     }
 }
 
-// 다른 문제 풀었던 기억을 가지고 짰지만 80% 정도의 정답률을 가진 코드
+// 다른 문제 풀었던 기억을 가지고 짰지만 80% 정도의 정답률을 가진 코드(3개 틀림)
 class Solution {
     fun solution(x: Int, y: Int, n: Int): Int {
         var dp = IntArray(y + 1) { 0 }
@@ -76,5 +76,41 @@ class Solution {
         }
 
         return if (dp[y] == 0) -1 else dp[y]
+    }
+}
+
+// 다 해결한 줄 알았지만 90% 정도의 정답률을 가진 코드(2개 틀림)
+class Solution {
+    fun solution(x: Int, y: Int, n: Int): Int {
+        var dp = IntArray(y + 1) { 1000000 }
+
+        if (x == y) return 0
+
+        dp[x] = 0
+        for (i in x + 1..y) {
+            if (i - n >= x && (i - x) % n == 0) dp[i] = dp[i - n] + 1
+            if (i % 2 == 0) dp[i] = minOf(dp[i], dp[i / 2] + 1)
+            if (i % 3 == 0) dp[i] = minOf(dp[i], dp[i / 3] + 1)
+        }
+
+        return if (dp[y] == 1000000) -1 else dp[y]
+    }
+}
+
+// 해결된 코드...휴
+class Solution {
+    fun solution(x: Int, y: Int, n: Int): Int {
+        var dp = IntArray(y + 1) { 1000000 }
+
+        if (x == y) return 0
+
+        dp[x] = 0
+        for (i in x + 1..y) {
+            if (i - n >= x) dp[i] = minOf(dp[i], dp[i - n] + 1)
+            if (i % 2 == 0) dp[i] = minOf(dp[i], dp[i / 2] + 1)
+            if (i % 3 == 0) dp[i] = minOf(dp[i], dp[i / 3] + 1)
+        }
+
+        return if (dp[y] == 1000000) -1 else dp[y]
     }
 }
