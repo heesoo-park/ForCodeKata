a8b015c3a138447034ffd87aa42e8d9fcea49b16
heesoo-park <80674868+heesoo-park@users.noreply.github.com>
2024-06-19 18:12:45 +0900

    Create Main.kt

---
+++ baekjoon 문제집/24337 가희와 탑/Main.kt
@@ -0,0 +1,34 @@
+import java.io.*
+import java.util.*
+
+fun main(args: Array<String>) {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val sb = StringBuilder()
+    
+    val (n, a, b) = br.readLine().split(' ').map { it.toInt() }
+    val buildings = mutableListOf<Int>()
+    
+    if (a + b > n + 1) {
+        print(-1)
+        return
+    }
+    
+    for (i in 1 until a) {
+        buildings.add(i)
+    }
+    
+    buildings.add(maxOf(a, b))
+    
+    for (i in b - 1 downTo 1) {
+        buildings.add(i)
+    }
+    
+    while (buildings.size < n) {
+        if (a == 1) buildings.add(1, 1) else buildings.add(0, 1)
+    }
+    
+    for (building in buildings) {
+        sb.append("$building ")
+    }
+    print(sb)
+}
