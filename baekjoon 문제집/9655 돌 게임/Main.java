4095ffe23c25c657bb509768a2a79360f881d1ce
heesoo-park <80674868+heesoo-park@users.noreply.github.com>
2024-07-03 09:52:02 +0900

    Update Main.java

--- baekjoon 문제집/9655 돌 게임/Main.java
+++ baekjoon 문제집/9655 돌 게임/Main.java
@@ -1,3 +1,27 @@
+// DP를 활용한 코드
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    
+    static boolean[] dp = new boolean[1001];
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int n = Integer.parseInt(br.readLine());
+        
+        dp[1] = true;
+        dp[3] = true;
+        for (int i = 4; i <= n; i++) {
+            dp[i] = (i % 3 == 0) ? !dp[i - 3] : !dp[i - 1];
+        }
+        
+        System.out.print(dp[n] ? "SK" : "CY");
+    }
+}
+
+// 숫자 자체를 활용한 코드
 import java.io.*;
 import java.util.*;
 
