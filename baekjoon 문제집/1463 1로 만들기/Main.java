dcd205f57cb713fc8ae20d47f76f5cbb9ad19268
heesoo-park <80674868+heesoo-park@users.noreply.github.com>
2024-07-03 10:13:16 +0900

    Update Main.java

--- baekjoon 문제집/1463 1로 만들기/Main.java
+++ baekjoon 문제집/1463 1로 만들기/Main.java
@@ -1,3 +1,29 @@
+// 조금 더 빠른 코드
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    
+    static int[] dp = new int[1000001];
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int n = Integer.parseInt(br.readLine());
+        
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + 1;
+            
+            if (i % 2 == 0) dp[i] = Math.min(dp[i], dp[i / 2] + 1);
+            
+            if (i % 3 == 0) dp[i] = Math.min(dp[i], dp[i / 3] + 1);
+        }
+        
+        System.out.print(String.valueOf(dp[n]));
+    }
+}
+
+
 import java.io.*;
 import java.util.*;
 
