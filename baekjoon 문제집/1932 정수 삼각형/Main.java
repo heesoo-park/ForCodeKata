9f8d0630e5790396dd235f22ada88c34d1a758ae
heesoo-park <80674868+heesoo-park@users.noreply.github.com>
2024-07-12 11:17:39 +0900

    Update Main.java

--- baekjoon 문제집/1932 정수 삼각형/Main.java
+++ baekjoon 문제집/1932 정수 삼각형/Main.java
@@ -1,3 +1,47 @@
+// 탑다운 방식
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    
+    private static int[][] triangle;
+    private static int[][] dp;
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        
+        int n = Integer.parseInt(br.readLine());
+        triangle = new int[n + 1][n + 1];
+        dp = new int[n + 1][n + 1];
+        
+        for (int i = 1; i <= n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= i; j++) {
+                triangle[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        
+        for (int i = 1; i <= n; i++) {
+            dp[n][i] = triangle[n][i];
+        }
+        
+        System.out.print(findMaxSum(n, 1, 1));
+    }
+    
+    static int findMaxSum(int n, int depth, int idx) {
+        if (depth == n) return dp[depth][idx];
+        
+        if (dp[depth][idx] == 0) {
+            dp[depth][idx] = Math.max(findMaxSum(n, depth + 1, idx), findMaxSum(n, depth + 1, idx + 1)) + triangle[depth][idx];
+        }
+        
+        return dp[depth][idx];
+    }
+}
+
+
+// 바텀업 방식
 import java.io.*;
 import java.util.*;
 
