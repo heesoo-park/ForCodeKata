0ae94301249f6e4f2f7d2678c0619431d230b4f5
heesoo-park <80674868+heesoo-park@users.noreply.github.com>
2024-07-15 11:37:16 +0900

    Update Main.java

--- baekjoon 문제집/10844 쉬운 계단 수/Main.java
+++ baekjoon 문제집/10844 쉬운 계단 수/Main.java
@@ -1,3 +1,44 @@
+// 탑다운 방식
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    
+    private static long[][] dp = new long[101][11];
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        for (int i = 0; i < 10; i++) {
+            dp[1][i] = 1;            
+        }
+        
+        int n = Integer.parseInt(br.readLine());
+        
+        long sum = 0;
+        for (int i = 0; i < 10; i++) {
+            sum += solve(n, i);
+        }
+        System.out.print(sum % 1000000000);
+    }
+    
+    static long solve(int n, int i) {
+        if (n == 1) {
+            return (i != 0) ? 1 : 0;
+        }
+        
+        if (dp[n][i] != 0) return dp[n][i];
+        
+        if (i == 0) dp[n][i] = solve(n - 1, i + 1);
+        else if (i == 9) dp[n][i] = solve(n - 1, 8);
+        else dp[n][i] = (solve(n - 1, i - 1) + solve(n - 1, i + 1)) % 1000000000;
+        
+        return dp[n][i];
+    }
+}
+
+
+// 바텀업 방식
 import java.io.*;
 import java.util.*;
 
