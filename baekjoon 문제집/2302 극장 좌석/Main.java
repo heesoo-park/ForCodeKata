f65e6749ad381425034cd99d84b155d4b15295d3
heesoo-park <80674868+heesoo-park@users.noreply.github.com>
2024-07-16 10:11:02 +0900

    Update Main.java

--- baekjoon 문제집/2302 극장 좌석/Main.java
+++ baekjoon 문제집/2302 극장 좌석/Main.java
@@ -1,3 +1,45 @@
+// 탑다운 방식
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    
+    private static long[] dp = new long[41];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+        
+        dp[0] = 1;
+        dp[1] = 1;
+        dp[2] = 2;
+        
+        int prev = 0;
+        long result = 1;
+        while (m-- > 0) {
+            int ticket = Integer.parseInt(br.readLine());
+            result *= solve(ticket - prev - 1);
+            prev = ticket;
+        }
+        
+        result *= solve(n - prev);
+        System.out.print(result);
+    }
+    
+    static long solve(int range) {
+        if (range < 2) return 1;
+        else if (range == 2) return 2;
+        
+        if (dp[range] != 0) return dp[range];
+        
+        dp[range] = solve(range - 1) + solve(range - 2);
+        
+        return dp[range];
+    }
+}
+
+// 바텀업 방식
 import java.io.*;
 import java.util.*;
 
