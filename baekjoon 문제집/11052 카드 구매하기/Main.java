7156354e70ddf3638fac030b666d825bd94ff9a2
heesoo-park <80674868+heesoo-park@users.noreply.github.com>
2024-07-17 11:31:00 +0900

    Update Main.java

--- baekjoon 문제집/11052 카드 구매하기/Main.java
+++ baekjoon 문제집/11052 카드 구매하기/Main.java
@@ -1,3 +1,37 @@
+// 탑다운 방식
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    private static int[] price = new int[1001];
+    private static int[] dp = new int[1001];
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        
+        int n = Integer.parseInt(br.readLine());
+        
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= n; i++) {
+            price[i] = Integer.parseInt(st.nextToken());
+        }
+        
+        System.out.print(solve(n));
+    }
+    
+    static int solve(int n) {
+        if (dp[n] > 0) return dp[n];
+        
+        for (int i = 1; i <= n; i++) {
+            dp[n] = Math.max(dp[n], solve(n - i) + price[i]);
+        }
+        
+        return dp[n];
+    }
+}
+
+// 바텀업 방식
 import java.io.*;
 import java.util.*;
 
