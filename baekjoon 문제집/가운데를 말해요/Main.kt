841ef4db256f18f8ab2d72fc62f39f595374e41e
heesoo-park <80674868+heesoo-park@users.noreply.github.com>
2024-08-14 11:49:07 +0900

    Update Main.kt

--- baekjoon 문제집/가운데를 말해요/Main.kt
+++ baekjoon 문제집/가운데를 말해요/Main.kt
@@ -1,3 +1,53 @@
+// 재풀이한 코드
+// 전에 풀었던 것보다 30퍼가량 빨라짐
+import java.io.*
+import java.util.*
+
+fun main(args: Array<String>) = with(BufferedReader(InputStreamReader(System.`in`))){
+    val sb = StringBuilder()
+    val n = readLine().toInt()
+
+    val lessMid = PriorityQueue<Int>(reverseOrder())
+    val moreMid = PriorityQueue<Int>()
+    var mid = 0
+    for (i in 1..n) {
+        if (i == 1) {
+            mid = readLine().toInt()
+            sb.append(mid).append('\n')
+            continue
+        }
+
+        val cur = readLine().toInt()
+        if (mid > cur) {
+            lessMid.offer(cur)
+        } else {
+            moreMid.offer(cur)
+        }
+
+        if (i % 2 == 0) {
+            if (lessMid.size > moreMid.size) {
+                sb.append(minOf(lessMid.peek(), mid))
+            } else {
+                sb.append(minOf(moreMid.peek(), mid))
+            }
+        } else {
+            if (lessMid.size > moreMid.size) {
+                moreMid.offer(mid)
+                mid = lessMid.poll()
+            } else if (lessMid.size < moreMid.size) {
+                lessMid.offer(mid)
+                mid = moreMid.poll()
+            }
+
+            sb.append(mid)
+        }
+        sb.append('\n')
+    }
+
+    print(sb)
+}
+
+
 // 통과한 코드
 // 우선순위큐 두 개(오름차순, 내림차순)를 쓰게 됐다.
 // println을 너무 써서 시간초과가 나더라
